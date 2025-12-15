37fc2654f2b5844e4a28681736f8ec9982b02029
AndresGiraldoB <andres.gb1986@outlook.es>
2025-12-15 12:09:39 +0100

    ""

--- DWEC/bloque_2/Angular/week_10/angular-inmosanvi_services/src/app/property-form/property-form.ts
+++ DWEC/bloque_2/Angular/week_10/angular-inmosanvi_services/src/app/property-form/property-form.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component, output, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, output } from '@angular/core';
 import { Property } from '../interfaces/property';
 import { FormsModule, NgForm } from '@angular/forms';
+import { EncodeBase64Directive } from '../../directives/encode-base64-directive';
 
 @Component({
   selector: 'property-form',
-  imports: [FormsModule],
+  imports: [FormsModule, EncodeBase64Directive],
   templateUrl: './property-form.html',
   styleUrl: './property-form.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -26,33 +27,13 @@ export class PropertyForm {
       mainPhoto:"",
   };
   //imagePreview signal
-  imagePreview=signal("");
-
-
-  changeImagen(fileInput: HTMLInputElement) {
-    if(!fileInput.files || fileInput.files.length === 0){
-      //regresamos mainPhoto ""
-      //this.newProperty.mainPhoto="";
-      return;
-    }
-    //si todo va bien
-    const reader: FileReader=new FileReader();
-    reader.readAsDataURL(fileInput.files[0]);
-    reader.addEventListener("loadend",()=>{
-      //extraemos el reader.result como string
-      //this.newProperty.mainPhoto=reader.result as string;
-      this.imagePreview.set(reader.result as string);
-      return;
-    })
-    
-  }
-
+  //imagePreview=signal("");
   //addProduct emit
   addProperty(productForm:NgForm){
-    this.newProperty.mainPhoto=this.imagePreview();
+    //this.newProperty.mainPhoto=this.imagePreview();
     this.added.emit({...this.newProperty});
     productForm.resetForm();
-    this.imagePreview.set("");
+    //this.imagePreview.set("");
     this.newProperty.mainPhoto="";
 
   }
